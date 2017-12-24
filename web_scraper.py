e84d39bdbf2aa5326e417bc6c90c8146db1adc3c
Lewin Appleton-Fox <lewin.a.f@gmail.com>
2017-12-24 01:07:56 +0000

    Add BeatifulSoup

--- web_scraper.py
+++ web_scraper.py
@@ -1,5 +1,16 @@
 # Scrape Instagram for images
+from bs4 import BeautifulSoup
+import requests
 
 tag = str.lower(input("Enter your hashtag: \n"))
-url = "http://www.instagram.com/tags/" + tag
+url = "http://www.instagram.com/tags/{}".format(tag)
 print("Searching " + url)
+
+# Retrieve URL data
+page = requests.get(url)
+print("Requests status code {}".format(page.status_code))
+
+# Convert to BeautifulSoup object
+soup = BeautifulSoup(page.content, 'html.parser')
+# Extract body
+body = soup.body
